ab4429d1ad78d7d7920451799dfa2be61de9cb2e
Nihaal Manesia <nihaal419@gmail.com>
2016-10-01 21:18:54 -0500

    Init 2.0

--- Happy Wallet/Editing.swift
+++ Happy Wallet/Editing.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  Editing.swift
 //  Happy Wallet
 //
 //  Created by Nihaal Manesia on 10/1/16.
@@ -8,28 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var netLabel: UILabel!
-    @IBOutlet weak var monthlyLabel: UILabel!
-    @IBOutlet weak var expenseLabel: UILabel!
-    @IBOutlet weak var moveButton: UIButton!
+class Editing: UIViewController {
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    
+    @IBAction func moveBack(_ sender: AnyObject) {
+        navigationController?.popToRootViewController(animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        netLabel.text = "$0.00"
-        monthlyLabel.text = "$0.00"
-        expenseLabel.text = "$0.00"
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
